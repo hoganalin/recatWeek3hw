74a07deb383960108f9919d44d46f5f1f5900143
Rogan <hoganalin@gmail.com>
2025-02-01 10:29:26 +0800

    更新內容

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/recatWeek3hw/",
+  // 填入你的 GitHub repository 名稱
   plugins: [react()],
+  build: {
+    outDir: 'dist', // 確保輸出資料夾是 dist
+  },
 })
